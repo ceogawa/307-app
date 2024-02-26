fc6114280db4e03673d6f02188b198f033f35b22
ceogawa <ceogawa@calpoly.edu>
2024-02-26 14:15:57 -0800

    Delete Entries option

---
+++ packages/react-frontend/src/MyApp.js
@@ -0,0 +1,43 @@
+// src/MyApp.js
+import React, { useState } from "react";
+import Table from "./Table";
+
+function MyApp() {
+  const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor"
+    },// the rest of the data
+    {
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]);
+
+  return (
+    <div className="container">
+      <Table
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+      />
+    </div>
+  );
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+
+}
+
+export default MyApp;
